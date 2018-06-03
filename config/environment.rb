14788cfe83ec1ffc2923cb810beae8b058d92121
Ji Yim <gsooyim@gmail.com>
2018-06-03 17:13:23 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,12 @@
 require 'sqlite3'
 
-
+# create database
 DB = {:conn => SQLite3::Database.new("db/songs.db")}
+
+# drop songs to avoid errors
 DB[:conn].execute("DROP TABLE IF EXISTS songs")
 
+# create songs table
 sql = <<-SQL
   CREATE TABLE IF NOT EXISTS songs (
   id INTEGER PRIMARY KEY,
@@ -13,4 +16,6 @@ sql = <<-SQL
 SQL
 
 DB[:conn].execute(sql)
+
+# Do not return database rows as array but as hash with column names as keys
 DB[:conn].results_as_hash = true
